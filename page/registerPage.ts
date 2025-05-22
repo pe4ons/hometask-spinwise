275c22b16bfb8525dc907946d919cc25e2184658
Peteris <peteris.cibula@gmail.com>
2025-05-22 14:47:49 +0300

    add test

--- page/registerPage.ts
+++ page/registerPage.ts
@@ -1,12 +1,34 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test';
+import 'dotenv/config';
 
 export class RegisterPage {
-    
-  constructor(public readonly page: Page) {
+    readonly username: Locator;
+    readonly email: Locator;
+    readonly password: Locator;
+    readonly confirmPassword: Locator;
+    readonly registerBtn: Locator;
 
-  }
+    constructor(public readonly page: Page) {
+        this.username = page.locator('input[name="username"]');
+        this.email = page.locator('input[name="email"]');
+        this.password = page.locator('input[name="password"]');
+        this.confirmPassword = page.locator('input[name="confirmPassword"]');
+        this.registerBtn = page.getByRole('button', { name: 'Register' });
+    }
 
-  async openRegisterPage() {
-    await this.page.goto('hhttps://auth-home-task.vercel.app/register');
-  }
+    async openRegisterPage() {
+        await this.page.goto('https://auth-home-task.vercel.app/register');
+    }
+
+    async fillRegistrationForm(userName: string, email: string) {
+        const password = process.env.USER_PASSWORD;
+        if (!password) {
+            throw new Error('password not set in .env file')
+        }
+
+        await this.username.fill(userName);
+        await this.email.fill(email);
+        await this.password.fill(password);
+        await this.confirmPassword.fill(password);
+    }
 }
--- tests/registerPage.spec.ts
+++ tests/registerPage.spec.ts
@@ -4,5 +4,10 @@ import { RegisterPage } from '../page/registerPage';
 test.describe('registraion page tests', () => {
     test('Successful registration using a valid username, email, and password', async ({ page }) => {
         const registerPage = new RegisterPage(page);
+
+        await registerPage.openRegisterPage();
+        await registerPage.fillRegistrationForm('Mike', 'mike@mike.com');
+        await registerPage.registerBtn.click();
+        await expect(page.getByText('You have registered successfully!')).toBeVisible();
     })
 })
