62f7e38d84cd955240626a87ec9505900b1e1123
Peteris <peteris.cibula@gmail.com>
2025-05-22 14:56:16 +0300

    fix

--- tests/registerPage.spec.ts
+++ tests/registerPage.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { RegisterPage } from '../page/registerPage';
 
-test.describe('registraion page tests', () => {
+test.describe('Registraion page tests', () => {
     test('Successful registration using a valid username, email, and password', async ({ page }) => {
         const registerPage = new RegisterPage(page);
 
