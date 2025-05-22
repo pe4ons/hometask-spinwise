b343980702dbfa29ce84c2ef14a508bd49c468bd
Peteris <peteris.cibula@gmail.com>
2025-05-22 14:59:47 +0300

    fix typo

--- tests/registerPage.spec.ts
+++ tests/registerPage.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
-import { RegisterPage } from '../page/registerPage';
+import { RegistrationPage } from '../page/registerPage';
 
-test.describe('registraion page tests', () => {
+test.describe('Registration page tests', () => {
     test('Successful registration using a valid username, email, and password', async ({ page }) => {
-        const registerPage = new RegisterPage(page);
+        const registerPage = new RegistrationPage(page);
 
         await registerPage.openRegisterPage();
         await registerPage.fillRegistrationForm('Mike', 'mike@mike.com');
